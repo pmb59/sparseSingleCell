b1079f586e37cd104d1399e5c27d0655d509adeb
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2018-12-16 17:33:30 +0000

    Update Sushi-bedgraphplot.R

--- Sushi-bedgraphplot.R
+++ Sushi-bedgraphplot.R
@@ -2,25 +2,21 @@ library('Sushi')
 
 bw <- read.table('ENCFF974HWL.bedGraph', head=FALSE)
 colnames(bw) <- c('chrom' ,'start' ,'end', 'value')
-head(bw)
 
-chrom= "chr7"
-chromstart= 144861386-20000
-chromend= 144861386+20000
-
-plotBedgraph(bw,chrom,chromstart,chromend,colorbycol= SushiColors(5),linecolor ='black',addscale = FALSE)
-labelgenome(chrom,chromstart,chromend,n=4,scale="Mb")
-mtext("Read-depth normalized signal",side=2,line=1.75,cex=1,font=2)
-axis(side=2,las=2,tcl=.2)
+#Fgf4 (mm10)
+chrom = "chr7"
+chromstart = 144861386-20000
+chromend = 144861386+20000
 
 pdf('fgf4.pdf', width=8, height=5)
 layout(matrix(c(1,1,2,2),2, 2, byrow = TRUE))
 par(mar=c(3,4,1,1))
+
 plotBedgraph(bw,chrom,chromstart,chromend,colorbycol= SushiColors(5),linecolor ='black',addscale = FALSE)
 labelgenome(chrom,chromstart,chromend,n=4,scale="Mb")
 mtext("Read-depth normalized signal",side=2,line=1.75,cex=0.8,font=2)
 axis(side=2,las=2,tcl=.2)
-zoomregion1      = c(144861386-500,144861386+500)
+zoomregion1 = c(144861386-500,144861386+500)
 zoomsregion(zoomregion1,extend=c(0.01,0.13),wideextend=0.05, offsets=c(0,0))
 
 plotBedgraph(bw,chrom,chromstart=zoomregion1[1], chromend=zoomregion1[2],colorbycol= SushiColors(5), linecolor ='black')
