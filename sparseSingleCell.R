e649d88d2fa4c21fbeefb39bb65e0e57d9ae1313
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2018-12-21 02:16:07 +0000

    Update sparseSingleCell.R

--- sparseSingleCell.R
+++ sparseSingleCell.R
@@ -50,16 +50,16 @@ for (j in 1:length(Lt) ){
 #---------------------------------
 # fdapace
 #---------------------------------
-pace <- FPCA(Ly=Ly_sorted, Lt=Lt_sorted, optns = list(maxK=30, nRegGrid=100, plot=TRUE) )
+pace <- FPCA(Ly=Ly_sorted, Lt=Lt_sorted, optns = list(maxK=30, nRegGrid=100, plot=TRUE, outPercent=c(0.06, 1 )) )
 
-pdf('design_plot.pdf',height=6, width=6)
+pdf('Fig1_design_plot.pdf',height=6, width=6)
 CreateDesignPlot(Lt_sorted, obsGrid = NULL, isColorPlot = TRUE, noDiagonal = TRUE, addLegend = TRUE)
 dev.off()
 
 library(wesanderson)
 cellcolor <- wes_palette("BottleRocket2",n=5, type="discrete")
 
-pdf('FIGURE1.pdf',height=5, width=7)
+pdf('Fig2.pdf',height=5, width=7)
 par(mfrow=c(2,3))
 par(mar=c(5,4,2,1))
 for (i in 2:5){
@@ -67,6 +67,6 @@ for (i in 2:5){
     if (i==2 | i==3) CreatePathPlot( pace, K=9, subset = i, main = "", pch = 16, showMean=FALSE,col=cellcolor[i-1], ylab='GpC accessibility',xlab='chr7',main=paste('cell ', i-1) )
 }
 CreateScreePlot(pace)
-CreateFuncBoxPlot(pace, xlab = 'chr7', ylab = 'GpC accessibility',main='Functional box-plot',col='red', optns = list(variant='bagplot'))
+CreateFuncBoxPlot(pace, xlab = 'chr7', ylab = 'GpC accessibility',main='Functional box-plot', optns = list(variant='pointwise'))
 dev.off()
 
